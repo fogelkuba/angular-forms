7ee212777ee5a778f4dfe0f57149100df8585850
fogelkuba <fogelkuba@gmail.com>
2018-08-05 13:57:17 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted: boolean = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -38,6 +46,12 @@ export class AppComponent {
   onSubmit() {
     console.log('alt');
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
 
+    this.submitted = true;
   }
 }
